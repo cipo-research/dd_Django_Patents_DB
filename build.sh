e584bc597c30fe0370bbb63591ad59af1d873d08
Dylan Higuchi <dylan.higuchi@ised-isde.gc.ca>
2023-09-14 16:10:18 -0400

    various setting changes

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-#set -o errexit
-
-ls ../
-source ../Scripts/activate
-python manage.py migrate
--- patentproject/settings.py
+++ patentproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_p*ym-w$+iuh=n&2-0s0r@d=)xhh2!bq#67-aagu3it^7j8fgp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR/'static',]
+# STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
