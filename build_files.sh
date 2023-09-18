72986c1baa6e0a53d897b9fd27f922b0219d869c
Dylan Higuchi <dylan.higuchi@ised-isde.gc.ca>
2023-09-18 12:40:39 -0400

    commented out makemigrations command from build script

--- build_files.sh
+++ build_files.sh
@@ -5,7 +5,7 @@ echo "Building the project..."
 pip install -r requirements.txt
 
 echo "Make Migration..."
-python manage.py makemigrations --noinput
+#python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 #echo "Collect Static..."
